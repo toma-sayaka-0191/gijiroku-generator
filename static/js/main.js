88efb326ecb35408574e3768445d5764c90582da
toma-sayaka-0191 <toma.sayaka.0191@gmail.com>
2021-12-29 17:46:41 +0900

    202112291746

--- static/js/main.js
+++ static/js/main.js
@@ -8,26 +8,36 @@ const tbody = document.getElementById('tbody');
 let cnt = 0;
 let player;
 let dla;
+let flg = false;
 
 const recorder = new Recorder()
 
 // start button
 startButton.addEventListener('click', function () {
+
+    let url = ""
+
     recorder.start()
 
-    cnt+= 1;
-    console.log(cnt)
-    tbody.insertAdjacentHTML('beforeend', "<tr><td><audio id='player" + cnt + "' controls src=''></audio></td><td><a id='dl" + cnt + "'>DL</a></td></tr>")
-    player = document.getElementById('player' + cnt);
-    dla = document.getElementById('dl' + cnt);
+    do {
+        cnt+= 1;
+        tbody.insertAdjacentHTML('beforeend', "<tr><td><audio id='player" + cnt + "' controls src=''></audio></td><td><a id='dl" + cnt + "'>DL</a></td></tr>")
+        player = document.getElementById('player' + cnt);
+        dla = document.getElementById('dl' + cnt);
 
-    player.src = ""
+        url = recorder.stop()
+        player.src = url
+        dla.href = url
+        dla.download = 'voice_' + cnt + '.wav';
+
+    } while (flg = false);
 });
 
 // stop button
 stopButton.addEventListener('click', function () {
-    const url = recorder.stop()
-    player.src = url
-    dla.href = url
-    dla.download = 'test.wav';
+    flg = true
+    //const url = recorder.stop()
+    //player.src = url
+    //dla.href = url
+    //dla.download = 'voice_' + cnt + '.wav';
 });
