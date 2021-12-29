5252a7d32c4e33998a4417425e7d3e8e10cd05a6
toma-sayaka-0191 <toma.sayaka.0191@gmail.com>
2021-12-29 13:23:34 +0900

    202112291323

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+import Recorder from "./recorder.js"
+
+const player = document.getElementById('player');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const dla = document.getElementById('dl');
+
+const recorder = new Recorder()
+
+// start button
+startButton.addEventListener('click', function () {
+    player.src = ""
+    recorder.start()
+});
+
+// stop button
+stopButton.addEventListener('click', function () {
+    const url = recorder.stop()
+    console.log(url)
+    console.log(url)
+    player.src = url
+    dla.href = url
+});
