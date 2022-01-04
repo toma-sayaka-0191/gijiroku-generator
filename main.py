123e03a7da82b0591a755c7b6511c344cbfd7b8a
toma-sayaka-0191 <toma.sayaka.0191@gmail.com>
2022-01-04 15:07:23 +0900

    202201041507

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, jsonify, abort, make_response, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+@app.route('/get', methods=['GET'])
+def get():
+    result = ""
+    return result
+
+@app.route('/post', methods=['POST'])
+def post():
+    result = request.form["param"]
+    return make_response(result)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,77 @@
-import Init from "./init.js"
-import Recorder from "./recorder.js"
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
 
+const tbody = document.getElementById('tbody');
 
-const init = new Init()
-const recorder = new Recorder()
+let cnt = 0;
+let url = '';
+let player;
+let dla;
+let stream;
+let flg = false;
 
 // start button
 startButton.addEventListener('click', async function () {
-    await recorder.start()
-    console.log("A")
+    await AddRow();
+    do {
+        stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+        player.srcObject = stream;
+    } while (flg = false);
+    stream.getTracks().forEach((track) => track.stop())
+
 });
 
 // stop button
 stopButton.addEventListener('click', function () {
-    init.flg = true
+    flg = True;
 });
+
+function AddRow(){
+    cnt+= 1
+    tbody.insertAdjacentHTML('beforeend', "<tr><td><audio id='player" + cnt + "' controls src=''></audio></td><td><a id='dl" + cnt + "'>DL</a></td></tr>")
+    player = document.getElementById('player' + cnt)
+    dla = document.getElementById('dl' + cnt)
+};
+  
+
+/* async function start() {
+    stream = (await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    }))
+    console.log(this.wavSamples)
+    this.captureStart()
+};
+ */
+
+/* function getFunc(url) {
+    fetch(url)
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(text) {
+
+    });
+}
+
+function post_func(url) {
+    // Postで送るパラメータを作成
+    let formData = new FormData();
+    formData.append('param', document.getElementById('post_param').value);
+    fetch(url, {
+        method: 'POST',
+        body: formData,
+    })
+    .then(function() {
+        get_func('/get');
+    });
+} */
+
+
+/* function DoWhileStop(){
+    stream.getTracks().forEach((track) => track.stop())
+    url = recorder.stop()
+    player.src = url
+    dla.href = url
+    dla.download = 'voice_' + cnt + '.wav'
+}; */
