e303265aab4d5e131305108ff65b1d38f9a7294b
toma-sayaka-0191 <toma.sayaka.0191@gmail.com>
2022-01-06 00:14:51 +0900

    202201060014

--- static/js/main.js
+++ static/js/main.js
@@ -3,35 +3,33 @@ const stopButton = document.getElementById('stop');
 
 const tbody = document.getElementById('tbody');
 
-
-
 let cnt = 0;
 let url = '';
 let player;
 let dla;
-let flg;
-let chunks;
-let stream=[];
-
-const mr = new MediaRecorder(stream, {mimeType: 'audio/webm'})
+let mic, recorder, soundFile;
 
 
 // start button
 startButton.addEventListener('click', function () {
-    flg = false
-    AddRow()
-    start()
+    mic = new p5.AudioIn()
+    recorder = new p5.SoundRecorder();
+    soundFile = new p5.SoundFile();
+
+    mic.start()
+    recorder.setInput(mic);
+    recorder.record(soundFile);
 });
 
 // stop button
 stopButton.addEventListener('click', function () {
-    flg = true
-});
-
-mr.addEventListener('dataavailable', function(e){
-    if (e.data.size > 0) {
-        chunks.push(e.data)
-    }
+    let url
+    recorder.stop();
+    mic.stop()
+    url = saveSound(soundFile, 'voice_' + cnt + '.wav')
+    player.src = url
+    dla.href = url
+    dla.download = 'voice_' + cnt + '.wav'
 });
 
 function AddRow(){
@@ -39,48 +37,4 @@ function AddRow(){
     tbody.insertAdjacentHTML('beforeend', "<tr><td><audio id='player" + cnt + "' controls src=''></audio></td><td><a id='dl" + cnt + "'>DL</a></td></tr>")
     player = document.getElementById('player' + cnt)
     dla = document.getElementById('dl' + cnt)
-};
-  
-function start() {
-    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-    .then(function(s){
-        stream = s
-    })
-};
-
-function stop() {
-    stream.getTracks().forEach((track) => track.stop())
-    player.srcObject = stream
-};
-
-/* function getFunc(url) {
-    fetch(url)
-    .then(function(response) {
-        return response.text();
-    })
-    .then(function(text) {
-
-    });
 }
-
-function post_func(url) {
-    // Postで送るパラメータを作成
-    let formData = new FormData();
-    formData.append('param', document.getElementById('post_param').value);
-    fetch(url, {
-        method: 'POST',
-        body: formData,
-    })
-    .then(function() {
-        get_func('/get');
-    });
-} */
-
-
-/* function DoWhileStop(){
-    stream.getTracks().forEach((track) => track.stop())
-    url = recorder.stop()
-    player.src = url
-    dla.href = url
-    dla.download = 'voice_' + cnt + '.wav'
-}; */
