5e0eb9024c9a7137aa03f0ffb767cfe97115ca9f
toma-sayaka-0191 <toma.sayaka.0191@gmail.com>
2022-01-08 20:02:58 +0900

    202201082002

--- static/js/main.js
+++ static/js/main.js
@@ -38,8 +38,8 @@ stopButton.addEventListener('click', function () {
     lstream.getTracks().forEach(track => track.stop())
     url = window.URL.createObjectURL(new Blob(blobs),{type:"audio/webm"});
     player.src = url    
-    //processor.disconnect()
-    processor.onaudioprocess = null
+    processor.disconnect()
+    //processor.onaudioprocess = null
 //    dla.href = url
 //    dla.download = 'voice_' + cnt + '.wav'
 });
