2d16285b5e31af0fc1a3761aab4be32869401cf6
toma-sayaka-0191 <toma.sayaka.0191@gmail.com>
2022-01-08 21:03:19 +0900

    202201082103

--- static/js/main.js
+++ static/js/main.js
@@ -5,47 +5,119 @@ const tbody = document.getElementById('tbody');
 
 let cnt = 0;
 let player;
-let dla;
-let blobs;
-let lstream;
-let processor;
+let downloadLink;
+
+// for audio
+let audio_sample_rate = null;
+let scriptProcessor = null;
+let audioContext = null;
+
+// audio data
+let audioData = [];
+let bufferSize = 1024;
 
 // start button
 startButton.addEventListener('click', function () {
     AddRow()
-    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-    .then(function(stream) {
-        lstream=stream
-        
-        let context = new AudioContext()
-        let input = context.createMediaStreamSource(stream)
-        processor = context.createScriptProcessor(1024, 1, 1)
-
-        input.connect(processor)
-        processor.connect(context.destination)
-        processor.onaudioprocess = function(e) {
-            if (e.data && e.data.size > 0) {
-                blobs.push(e.data);
-            }
-            console.log(e.inputBuffer.getChannelData(0))
-        }
-    })
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess);
 });
 
 // stop button
 stopButton.addEventListener('click', function () {
-    let url
-    player.srcObject = blobs
-    lstream.getTracks().forEach(track => track.stop())  
-    processor.disconnect()
-    processor.onaudioprocess = null
-//    dla.href = url
-//    dla.download = 'voice_' + cnt + '.wav'
+    let url = exportWAV(audioData)
+    downloadLink.href = url
+    player.src = url
+    downloadLink.download = 'test.wav'
+    audioContext.close()
 });
 
 function AddRow(){
     cnt+= 1
-    tbody.insertAdjacentHTML('beforeend', "<tr><td><audio id='player" + cnt + "' controls src=''></audio></td><td><a id='dl" + cnt + "'>DL</a></td></tr>")
+    tbody.insertAdjacentHTML('beforeend', "<tr><td><audio id='player" + cnt + "' controls src=''></audio></td><td><a id='downloadLink" + cnt + "'>DL</a></td></tr>")
     player = document.getElementById('player' + cnt)
-    dla = document.getElementById('dl' + cnt)
+    downloadLink = document.getElementById('downloadLink' + cnt)
 }
+
+// getusermedia
+let handleSuccess = function (stream) {
+    audioContext = new AudioContext();
+    audio_sample_rate = audioContext.sampleRate;
+    console.log(audio_sample_rate);
+    scriptProcessor = audioContext.createScriptProcessor(bufferSize, 1, 1);
+    var mediastreamsource = audioContext.createMediaStreamSource(stream);
+    mediastreamsource.connect(scriptProcessor);
+    scriptProcessor.onaudioprocess = onAudioProcess;
+    scriptProcessor.connect(audioContext.destination);
+};
+
+// save audio data
+var onAudioProcess = function (e) {
+    var input = e.inputBuffer.getChannelData(0);
+    var bufferData = new Float32Array(bufferSize);
+    for (var i = 0; i < bufferSize; i++) {
+        bufferData[i] = input[i];
+    }
+    audioData.push(bufferData);
+};
+
+// export WAV from audio float data
+let exportWAV = function (audioData) {
+
+    let encodeWAV = function (samples, sampleRate) {
+    let buffer = new ArrayBuffer(44 + samples.length * 2);
+    let view = new DataView(buffer);
+
+    let writeString = function (view, offset, string) {
+        for (let i = 0; i < string.length; i++) {
+        view.setUint8(offset + i, string.charCodeAt(i));
+        }
+    };
+
+    let floatTo16BitPCM = function (output, offset, input) {
+        for (let i = 0; i < input.length; i++ , offset += 2) {
+        let s = Math.max(-1, Math.min(1, input[i]));
+        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+        }
+    };
+
+    writeString(view, 0, 'RIFF');  // RIFFヘッダ
+    view.setUint32(4, 32 + samples.length * 2, true); // これ以降のファイルサイズ
+    writeString(view, 8, 'WAVE'); // WAVEヘッダ
+    writeString(view, 12, 'fmt '); // fmtチャンク
+    view.setUint32(16, 16, true); // fmtチャンクのバイト数
+    view.setUint16(20, 1, true); // フォーマットID
+    view.setUint16(22, 1, true); // チャンネル数
+    view.setUint32(24, sampleRate, true); // サンプリングレート
+    view.setUint32(28, sampleRate * 2, true); // データ速度
+    view.setUint16(32, 2, true); // ブロックサイズ
+    view.setUint16(34, 16, true); // サンプルあたりのビット数
+    writeString(view, 36, 'data'); // dataチャンク
+    view.setUint32(40, samples.length * 2, true); // 波形データのバイト数
+    floatTo16BitPCM(view, 44, samples); // 波形データ
+
+    return view;
+    };
+
+    let mergeBuffers = function (audioData) {
+    let sampleLength = 0;
+    for (let i = 0; i < audioData.length; i++) {
+        sampleLength += audioData[i].length;
+    }
+    let samples = new Float32Array(sampleLength);
+    let sampleIdx = 0;
+    for (let i = 0; i < audioData.length; i++) {
+        for (let j = 0; j < audioData[i].length; j++) {
+        samples[sampleIdx] = audioData[i][j];
+        sampleIdx++;
+        }
+    }
+    return samples;
+    };
+
+    let dataview = encodeWAV(mergeBuffers(audioData), audio_sample_rate);
+    let audioBlob = new Blob([dataview], { type: 'audio/wav' });
+
+    let myURL = window.URL || window.webkitURL;
+    let url = myURL.createObjectURL(audioBlob);
+    return url;
+};
